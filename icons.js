211d14053d39726fa6a62ebe23d706d224e0d947
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2025-03-15 17:41:00 -0700

    initial build

---
+++ icons.js
@@ -0,0 +1,5 @@
+function GitHub_icon(){
+
+  document.getElementByClass("GitHub_icon").innerHTML = '<img src="./icons/brand-github.svg" width="50px" alt="GitHub Button">'
+
+}
---
+++ source/gitfriends_algorithm.js
@@ -0,0 +1,21 @@
+/*GitFriends Profile Algorithm*/
+
+var profiles = Array('@JacobBorgProgramming');
+
+
+function profileRec(){
+    var profiles_selected_1 = profiles[Math.floor(Math.random()*profiles.length)];
+    console.log(profiles_selected_1)
+
+    if (profiles_selected_1=='@JacobBorgProgramming') {
+      var types = Array('1','2')
+      var type = types[Math.floor(Math.random()*types.length)];
+
+      document.getElementById("outputtext").innerHTML = subject1+topic_libs+when+line_g_libs
+
+
+    }
+
+
+
+}
