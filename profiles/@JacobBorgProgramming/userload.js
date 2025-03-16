9ed0fc4821dba263a6172c093137f026b820a57d
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2025-03-15 17:55:40 -0700

    fixed links

---
+++ profiles/@JacobBorgProgramming/userload.js
@@ -0,0 +1,12 @@
+function user_load(){
+
+  /*Edit below line to update your profile's tagline*/
+  document.getElementById("profileTagline").innerHTML = 'Just a chill guy screaming (Programming) into the void (Cyberspace).'
+
+  /*Edit below line to update your profile's body content*/
+  document.getElementById("profileContent").innerHTML = ''
+
+  /*Below line will be updated by admin staff to reflect profile creation/update time*/
+  document.getElementById("profileTime").innerHTML = '5:22 PM - 15 Mar 2025'
+
+}
