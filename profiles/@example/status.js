59377f368ea0081027b347b627fe9e82111efa9b
JB <48691704+JacobBorgProgramming@users.noreply.github.com>
2025-04-04 11:32:29 -0700

    Update status.js

--- profiles/@example/status.js
+++ profiles/@example/status.js
@@ -1,5 +1,5 @@
 function user_status(){
   /*Edit below line to update your profile's status*/
-  document.getElementById("profileStatus").innerHTML = ''
+  document.getElementById("profileStatus").innerHTML = 'A test of the mobile editing'
 
 }
