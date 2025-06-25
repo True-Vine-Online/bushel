19eb60394e389157b26e8c5db294db2dffaefac7
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2025-06-24 19:33:14 -0700

    updated logo

--- profiles/@Bushel/status.js
+++ profiles/@Bushel/status.js
@@ -1,5 +1,5 @@
 function user_status(){
   /*Edit below line to update your profile's status*/
-  document.getElementById("profileStatus").innerHTML = 'A test of the mobile editing'
+  document.getElementById("profileStatus").innerHTML = 'Check out our new logo! (Just peep the top menu)'
 
 }
